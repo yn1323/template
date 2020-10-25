2950b5d77e2ce3b7e95c318b7b22172ce0d5ac47
yn1323 <yn1323@gmail.com>
2020-10-25 22:11:05 +0900

    ignore rule

--- app/.eslintrc.js
+++ app/.eslintrc.js
@@ -18,6 +18,7 @@ module.exports = {
     },
   },
   rules: {
-    '@typescript-eslint/explicit-function-return-type': 'off'
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/no-explicit-any': 'off'
   }
 };
