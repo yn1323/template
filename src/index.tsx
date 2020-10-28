071a95b91df94a0ed6ddb7aada3eec896d8bd3ab
yn1323 <yn1323@gmail.com>
2020-10-28 22:01:30 +0900

    router & scss

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { BrowserRouter } from 'react-router-dom'
 import Sample from 'src/page/Sample'
 import registerServiceWorker from 'src/reagisterServiceWorker'
+import 'src/asset/scss/index.scss'
 
 const run = () => {
   ReactDOM.render(
     <React.StrictMode>
-      <Sample test="test string" />
-      <div>hoge</div>
+      <BrowserRouter>
+        <Sample test="test string" />
+        <div>hoge</div>
+      </BrowserRouter>
     </React.StrictMode>,
     document.getElementById('root')
   )
   registerServiceWorker()
 }
 
+// eslint-disable-next-line no-prototype-builtins
 if (window.hasOwnProperty('cordova')) {
   document.addEventListener('deviceready', run, false)
 } else {
