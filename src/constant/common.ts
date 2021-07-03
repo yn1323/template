bba83cc7f43d3f6063bf8f47a691aa2d4ad6bb06
yn1323 <yn1323@gmail.com>
2021-07-03 17:30:31 +0900

    fix: storybook i18n

---
+++ src/constant/common.ts
@@ -0,0 +1,1 @@
+export const isCSR = typeof window !== 'undefined'
--- src/hooks/useLocale.ts
+++ src/hooks/useLocale.ts
@@ -1,8 +1,10 @@
+import { isCSR } from '@constant'
 import en from '@locales/en'
 import { useRouter } from 'next/router'
 
 export const useLocale = () => {
-  const { locale } = useRouter()
+  const locale = isCSR ? 'ja' : useRouter().locale
+
   const t = (text: any) => (locale === 'ja' ? text : (en as any)[text] || '')
 
   return { locale, t }
