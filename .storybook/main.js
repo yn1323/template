dc023b0d363fad7872f3cd5622469f43fe5be17c
yn1323 <yn1323@gmail.com>
2021-07-03 17:21:21 +0900

    fix: show ionic in story book

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,10 +1,18 @@
+const path = require('path')
+const { TsconfigPathsPlugin } = require('tsconfig-paths-webpack-plugin')
+
 module.exports = {
-  "stories": [
-    "../components/**/*.stories.mdx",
-    "../components/**/*.stories.@(js|jsx|ts|tsx)"
+  stories: [
+    '../components/**/*.stories.mdx',
+    '../components/**/*.stories.@(js|jsx|ts|tsx)',
   ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials"
-  ]
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+  //  resolve tsconfig alias
+  webpackFinal: async config => {
+    ;[].push.apply(config.resolve.plugins, [
+      new TsconfigPathsPlugin({ extensions: config.resolve.extensions }),
+    ])
+
+    return config
+  },
 }
