bbe0e7a8cc99df8cb8b3c2c93fa96b17c5d2e7a7
y-natani <yn1323@gmail.com>
2024-02-14 17:14:38 +0900

    fix: url

--- e2e/example.spec.ts
+++ e2e/example.spec.ts
@@ -1,6 +1,6 @@
 import { test } from "@playwright/test";
 
 test("test", async ({ page }) => {
-  await page.goto("http://localhost:3001/");
+  await page.goto("http://localhost:3010/");
   await page.getByRole("button", { name: "aaa" }).click();
 });
